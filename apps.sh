bbc2ca7cf6646f76054dfddfa047c73f29618405
Wojciech Sznapka <wojciech@sznapka.pl>
2014-05-23 04:42:06 -0700

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -5,7 +5,7 @@ sudo apt-get install curl vim tree htop terminator opnejdk-7-jdk httpie
 # lamp
 
 sudo apt-get install libapache2-mod-php5 
-sudo apt-get install  mysql-client mysql-server
+sudo apt-get install  mysql-client mysql-server mysql-workbench
 sudo mv composer.phar /usr/local/bin/composer
 sudo apt-get install npm nodejs nodejs-legacy
 
