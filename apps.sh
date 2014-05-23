2bf86fb921b887e6fcf7a909eebdbf4ed952300e
Wojciech Sznapka <wojciech@sznapka.pl>
2014-05-23 04:35:59 -0700

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -7,7 +7,7 @@ sudo apt-get install curl vim tree htop terminator opnejdk-7-jdk
 sudo apt-get install libapache2-mod-php5 
 sudo apt-get install  mysql-client mysql-server
 sudo mv composer.phar /usr/local/bin/composer
-sudo apt-get install npm
+sudo apt-get install npm nodejs nodejs-legacy
 
 
 
@@ -15,3 +15,5 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 sudo npm install -g grunt-cli
 sudo npm install -g bower
+
+sudo gem install compass
