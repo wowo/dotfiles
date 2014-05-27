2673be4b88368c253be39d9dff1e3df135060423
Wojciech Sznapka <wojciech@sznapka.pl>
2014-05-27 14:26:14 +0200

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install curl vim git tree htop terminator openjdk-7-jdk httpie multitail libapache2-mod-php5 mysql-client mysql-server mysql-workbench npm nodejs nodejs-legacy phpunit
+sudo apt-get install curl vim git tree htop terminator openjdk-7-jdk httpie multitail\
+  libapache2-mod-php5 phpunit mysql-client mysql-server mysql-workbench\
+  npm nodejs nodejs-legacy ruby 
 
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
